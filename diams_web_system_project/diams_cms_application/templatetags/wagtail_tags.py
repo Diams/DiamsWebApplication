17b42a799e6ecb00399781f489aada658f1da5d2
Masanao Daimon <diams.rider@gmail.com>
2024-06-02 08:38:56 +0900

    Fix link of header.

---
+++ diams_web_system_project/diams_cms_application/templatetags/wagtail_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from wagtail.models import Page
+from wagtail.query import PageQuerySet
+
+from home.models import HomePage
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_home_page():
+    try:
+        home_page_query: PageQuerySet = HomePage.objects
+        home_page = home_page_query.live().public().first()
+        home_page: HomePage = home_page.url
+        return home_page
+    except HomePage.DoesNotExist:
+        return "/"
