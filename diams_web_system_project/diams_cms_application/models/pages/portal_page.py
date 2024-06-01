e5a4b8227ce2a3db49be0837a4eb68813906c4af
Masanao Daimon <diams.rider@gmail.com>
2024-06-02 01:10:26 +0900

    Update to show categories also in portal page.

--- diams_web_system_project/diams_cms_application/models/pages/portal_page.py
+++ diams_web_system_project/diams_cms_application/models/pages/portal_page.py
@@ -15,3 +15,16 @@ class PortalPage(Page):
         FieldPanel("category"),
         FieldPanel("description"),
     ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        categories = Category.objects.all().order_by("priority")
+        portal_pages = PortalPage.objects.all()
+        navigation_categories = {
+            category.identify: (
+                category.screen_name,
+                portal_pages.filter(category=category),
+            ) for category in categories
+        }
+        context["navigation_categories"] = navigation_categories
+        return context
