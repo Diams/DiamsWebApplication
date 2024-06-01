ae3fbe8eeb2801196babebd21ac2bdc10156ca4b
Masanao Daimon <diams.rider@gmail.com>
2024-06-01 22:21:21 +0900

    Add portal page.

--- diams_web_system_project/diams_cms_application/models/portal_page.py
+++ diams_web_system_project/diams_cms_application/models/portal_page.py
@@ -2,10 +2,16 @@ from django.db import models
 from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
 
+from diams_cms_application.app_utils.categories import MAJOR_CATEGORIES
+
 
 class PortalPage(Page):
-    description = models.CharField(max_length=255)
+    parent_page_types = ['home.HomePage']
+    category = models.CharField(
+        max_length=255, choices=MAJOR_CATEGORIES, default=list(MAJOR_CATEGORIES.keys())[0])
+    description = models.TextField()
 
     content_panels = Page.content_panels + [
+        FieldPanel("category"),
         FieldPanel("description"),
     ]
