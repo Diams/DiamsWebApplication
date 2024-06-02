a2ef637d8e2d62e23d0740ccc4ac376eacb8d2ee
Masanao Daimon <diams.rider@gmail.com>
2024-06-02 09:54:48 +0900

    Register Category as a snipet.

--- diams_web_system_project/diams_cms_application/models/categories.py
+++ diams_web_system_project/diams_cms_application/models/categories.py
@@ -1,6 +1,8 @@
 from django.db import models
+from wagtail.snippets.models import register_snippet
 
 
+@register_snippet
 class Category(models.Model):
     priority = models.IntegerField(default=0)
     identify = models.CharField(max_length=255)
@@ -8,3 +10,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.screen_name
+
+    class Meta:
+        verbose_name_plural = "Categories"
