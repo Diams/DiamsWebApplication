b71561c34497a6ee7bb4a4914ba8173fa4f78a04
Masanao Daimon <diams.rider@gmail.com>
2024-06-16 17:10:55 +0900

    Create new page for other application.

--- diams_web_system_project/diams_cms_application/models/__init__.py
+++ diams_web_system_project/diams_cms_application/models/__init__.py
@@ -2,3 +2,4 @@ from diams_cms_application.models.pages.portal_page import PortalPage  # noqa: F
 from diams_cms_application.models.categories import Category  # noqa: F401
 from diams_cms_application.models.pages.a_content_page import AContentPage  # noqa: F401
 from diams_cms_application.models.pages.table_content_page import TableContentPage  # noqa: F401
+from diams_cms_application.models.pages.other_app_page import OtherAppPage  # noqa: F401
